375a47fe837e8d4a7e25d240915413e06ed662ab
John Barrett <johnjbarrett22@gmail.com>
2022-08-03 22:13:11 -0700

    Corrected typo in app

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,4 +1,4 @@
 print("Welcome to Band Name Generator!")
 city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
-print("Yout band name could be " + city_name + " " + pet_name + "!")
+print("Your band name could be " + city_name + " " + pet_name + "!")
